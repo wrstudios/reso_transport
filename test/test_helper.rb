3e076f07903ae16bdd5ffba2e55529634dd9ab38
Jonathan Silverman <jonathan@wr-studios.com>
2020-06-04 10:11:53 -0600

    upgrade faraday, it's a drop in replacement. and skip a few tests for now

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,7 @@ SECRETS = YAML.load_file("secrets.yml")
 
 require 'vcr'
 VCR.configure do |config|
+  # config.allow_http_connections_when_no_cassette = true
   config.cassette_library_dir = "test/vcr_cassettes"
   config.hook_into :faraday
 end
